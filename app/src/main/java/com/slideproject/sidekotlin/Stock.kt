0f0882dd884060e6d54d8f47bb19dc3bedb54c7a
liubangyao <a.today.13th.blackdays@gmail.com>
2021-03-21 21:58:19 +0800

    feat(new UI): tool attrubute
    tool attribute blogger 介紹

---
+++ app/src/main/java/com/slideproject/sidekotlin/Stock.kt
@@ -0,0 +1,8 @@
+package com.slideproject.sidekotlin
+
+class Stock {
+    var name:String? = null
+    var deal:String? = null
+    var quoteChange:String? = null
+    var increase:String? = null
+}
