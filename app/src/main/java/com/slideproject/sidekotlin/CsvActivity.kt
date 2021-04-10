bbbc04813f167d1ccb539e694cea53d9bc0682c5
liubangyao <a.today.13th.blackdays@gmail.com>
2021-04-10 20:50:26 +0800

    feat(new function): alertdialog
    write blog

--- app/src/main/java/com/slideproject/sidekotlin/CsvActivity.kt
+++ app/src/main/java/com/slideproject/sidekotlin/CsvActivity.kt
@@ -1,42 +1,84 @@
 package com.slideproject.sidekotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
 import android.os.Bundle
 import android.os.Environment
+import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.slideproject.sidekotlin.adapter.AssetAdapter
 import com.slideproject.sidekotlin.asset.AssetData
 import com.slideproject.sidekotlin.databinding.ActivityCsvBinding
+import com.slideproject.sidekotlin.databinding.AlertDialogCustomBinding
 import java.io.*
 import java.util.*
 import kotlin.collections.ArrayList
 
 class CsvActivity : AppCompatActivity() {
 
-    private val fileDirName = "csvfile"
-    private var completeExternalPath: File? = null
+    private fun showSimpleAlertDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Simple Alert Dialog")
+            .setMessage("Show Simple Button")
+            .setCancelable(false)
+            .setPositiveButton("OK") { dialog, which ->
+                Toast.makeText(applicationContext, "OK Button", Toast.LENGTH_LONG).show()
+            }
+            .setNegativeButton("Cancel") { dialog, which ->
+                Toast.makeText(applicationContext, "Cancel Button", Toast.LENGTH_LONG).show()
+            }
+            .setNeutralButton("Neutral") { dialog, which ->
+                Toast.makeText(applicationContext, "Neutral Button", Toast.LENGTH_LONG).show()
+            }
+            .show()
+    }
 
-    var layoutManager: RecyclerView.LayoutManager? = null
-    private lateinit var assetAdapterＣontainer: AssetAdapter
+    private fun showListAlertDialog() {
+        val languageList = arrayOf("中文", "英文", "日文", "德文", "法文")
+        AlertDialog.Builder(this)
+            .setTitle("請選擇應用程式語言")
+            .setItems(languageList) { dialog, which ->
+                Toast.makeText(
+                    applicationContext,
+                    "選擇程式語言 -> ${languageList[which]}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+            .setNegativeButton("取消") { dialog, which ->
+                Toast.makeText(applicationContext, "取消選擇語言完成", Toast.LENGTH_LONG).show()
+            }
+            .show()
+    }
 
-    private val CSV_HEADER = "rank, name, asset, company\n"
-    private val assetArray = Arrays.asList(
-        AssetData("1", "Elon Musk", "1.28兆", "特斯拉"),
-        AssetData("2", "Jeff Bezos", "1.22兆", "亞馬遜"),
-        AssetData("3", "Bernard Arnault", "7380億", "酩悅·軒尼詩－路易·威登集團"),
-        AssetData("4", "Bill Gates", "7120億", "微軟"),
-        AssetData("5", "Mark Zuckerberg", "6530億", "Facebook"),
-        AssetData("6", "Warren Buffett", "5890億", "波克夏"),
-        AssetData("7", "Zhong Shanshan", "5500億", "農夫山泉"),
-        AssetData("8", "穆凱什·阿姆巴尼", "5370億", "信實工業"),
-        AssetData("9", "貝特朗‧皮埃奇家族", "5180億", "愛馬仕"),
-        AssetData("10", "Steve Ballmer", "5180億", "微軟")
-    )
+    private fun showCustomViewAlertDialog() {
+        val bindingAlertDialog: AlertDialogCustomBinding =
+            AlertDialogCustomBinding.inflate(LayoutInflater.from(applicationContext))
+        createAlertDialog(
+            applicationContext,
+            bindingAlertDialog.getRoot(),
+            true
+        )
 
-    val mAssetList: ArrayList<AssetData> = ArrayList<AssetData>()
+        bindingAlertDialog.addButton.setOnClickListener(View.OnClickListener {
+            Log.d("TAG", "showCustomViewAlertDialog: " + bindingAlertDialog.editTextPersonName.text +
+                    "," + bindingAlertDialog.editTextPersonAssetCompany.text +
+                    "," + bindingAlertDialog.editTextPersonAsset.text)
+        })
+
+    }
+
+    private fun createAlertDialog(context: Context?, view: View?, isCancelable: Boolean): AlertDialog? {
+        val builder = AlertDialog.Builder(this@CsvActivity)
+        builder.setView(view)
+        val customDialog = builder.show()
+        customDialog.setCancelable(isCancelable)
+        return customDialog
+    }
 
     private lateinit var binding: ActivityCsvBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,6 +86,10 @@ class CsvActivity : AppCompatActivity() {
         binding = ActivityCsvBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.alertButton.setOnClickListener(View.OnClickListener {
+            showListAlertDialog()
+        })
 
         for (assets in assetArray) {
             mAssetList.add(assets)
@@ -108,6 +154,27 @@ class CsvActivity : AppCompatActivity() {
             binding.buttonSaveCsv.isEnabled = false
         }
     }
+
+    private val fileDirName = "csvfile"
+    private var completeExternalPath: File? = null
+    var layoutManager: RecyclerView.LayoutManager? = null
+    private lateinit var assetAdapterＣontainer: AssetAdapter
+
+    private val CSV_HEADER = "rank, name, asset, company\n"
+    private val assetArray = Arrays.asList(
+        AssetData("1", "Elon Musk", "1.28兆", "特斯拉"),
+        AssetData("2", "Jeff Bezos", "1.22兆", "亞馬遜"),
+        AssetData("3", "Bernard Arnault", "7380億", "酩悅·軒尼詩－路易·威登集團"),
+        AssetData("4", "Bill Gates", "7120億", "微軟"),
+        AssetData("5", "Mark Zuckerberg", "6530億", "Facebook"),
+        AssetData("6", "Warren Buffett", "5890億", "波克夏"),
+        AssetData("7", "Zhong Shanshan", "5500億", "農夫山泉"),
+        AssetData("8", "穆凱什·阿姆巴尼", "5370億", "信實工業"),
+        AssetData("9", "貝特朗‧皮埃奇家族", "5180億", "愛馬仕"),
+        AssetData("10", "Steve Ballmer", "5180億", "微軟")
+    )
+
+    val mAssetList: ArrayList<AssetData> = ArrayList<AssetData>()
 
     private val isExternalStorageReadOnly: Boolean
         get() {
